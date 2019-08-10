fbbd919e1e0c9e3412ac10155a808efdc6873389
Sunday Brian <brians931@gmail.com>
2019-08-10 11:10:05 +0300

    add a user model

---
+++ app/models.py
@@ -0,0 +1,37 @@
+from . import db,login_manager
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from datetime import datetime
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin,db.Model):
+    __tablename__='users'
+    
+    id=db.Column(db.Integer,primary_key=True)
+    profile_image=db.Column(db.String(64))
+    email=db.Column(db.String(64),unique=True,index=True)
+    username=db.Column(db.String(64),unique=True)
+    password_hash=db.Column(db.String(128))
+
+    # posts=db.relationship('BlogPost',backref='author',lazy='dynamic')
+
+    comments_user=db.relationship('Comment',backref='commenter',lazy="dynamic")
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self,password):
+        self.password_hash=generate_password_hash(password)
+
+    
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
+    
+    def __repr__(self):
+        return f'User {self.username}'    
