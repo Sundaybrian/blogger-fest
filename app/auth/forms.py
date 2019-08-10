3216e3ecf3047993758bbc890ab44eefcf005b95
Sunday Brian <brians931@gmail.com>
2019-08-10 13:04:19 +0300

    add methods to validate username and email while registering

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -12,6 +12,15 @@ class RegistrationForm(FlaskForm):
     pass_confirm=PasswordField('Confirm Password',validators=[DataRequired()])
     submit=SubmitField('Sign Up')
 
+    def validate_email(self,field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('There is an account with that email')
+
+    def validate_username(self,field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('That username is taken')    
+
+
 
 class LoginForm(FlaskForm):
     email=StringField('Your Email Address',validators=[DataRequired(),Email()])
@@ -19,3 +28,4 @@ class LoginForm(FlaskForm):
     remember=BooleanField('Remember me')
     submit=SubmitField('Sign In')
 
+    
