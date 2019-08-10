f188e3c88b650aa09f61d0321f2514b2d6b647ac
Sunday Brian <brians931@gmail.com>
2019-08-10 14:04:51 +0300

    add register html

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://sundaypriest:belter@localhost/blogfest'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    UPLOADED_PHOTOS_DEST ='app/static/photos
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 class ProdConfig(Config):
     '''
