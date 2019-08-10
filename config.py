ce75cdf36cdf7d99632e238526e2ec421a779612
Sunday Brian <brians931@gmail.com>
2019-08-10 10:09:04 +0300

    setup the config

---
+++ config.py
@@ -0,0 +1,38 @@
+import os
+
+class Config:
+    '''
+    General Configurations parent class
+    '''
+
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://sundaypriest:belter@localhost/blogfest'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST ='app/static/photos
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG=True
+    
+    
+config_options={
+    'development':DevConfig,
+    'production':ProdConfig 
+}    
