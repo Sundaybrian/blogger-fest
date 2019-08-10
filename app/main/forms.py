79c288afc3f61cfa96bff816c8ac06f1fe7155db
Sunday Brian <brians931@gmail.com>
2019-08-10 15:16:14 +0300

    add the update profile form class and methods to validate what the user is updating

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired,Email
+from . import User
 
 
 class PostForm(FlaskForm):
@@ -17,6 +18,26 @@ class CommentForm(FlaskForm):
     '''
     comment_content=TextAreaField('Comment',validators=[DataRequired()])
     submit=SubmitField('Submit')
+
+class UpdateProfile(FlaksForm):
+    '''
+    class to create a form to update user details
+    '''
+    email=StringField('Email',validators=[DataRequired(),Email()])
+    username=StringField('Username',validators=[DataRequired()])
+    submit=SubmitField('Update')
+
+    def validate_email(self,field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('There is an account with that email')
+
+    def validate_username(self,field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('That username is taken')    
+
+
+
+
     
     
-    
+
