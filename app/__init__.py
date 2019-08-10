e9f282b860fd4b16746a0f3701acd866145a015e
Sunday Brian <brians931@gmail.com>
2019-08-10 14:14:37 +0300

    created the main blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -21,9 +21,9 @@ def create_app(config_name):
     login_manager.init_app(app)
 
 
-    #registering the blueprint
-    # from .main import main as main_blueprint
-    # app.register_blueprint(main_blueprint)
+    # registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix='/authenticate')
