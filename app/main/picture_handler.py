79079adac1c55ec4e6e46724f0f523118c0235bc
Sunday Brian <brians931@gmail.com>
2019-08-11 10:44:51 +0300

    add the view fucntion to update a user details

--- app/main/picture_handler.py
+++ app/main/picture_handler.py
@@ -4,14 +4,14 @@ from flask import url_for,current_app
 
 def add_profile_pic(pic_upload,username):
 
-    filename=pic_upload.filename
-    #sunny '.' 'jpg'
-    ext_type=filename.split('.')[-1]
-
-    #username .jpg
-    storage_filename=str(username)+'.'+ext_type
+    filename=pic_upload.filename #sunny.jpg
+   
+    ext_type=filename.split('.')[-1]  #sunny '.' 'jpg'
+ 
+    storage_filename=str(username)+'.'+ext_type #username.jpg
 
     filepath=os.path.join(current_app.root_path,'static/photos',storage_filename)
+
     output_size=(200,200)
     pic=Image.open(pic_upload)
     pic.thumbnail(output_size)
--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,10 @@
-from flask import render_template,redirect,url_for,abort,flash
+from flask import render_template,redirect,url_for,abort,flash,request
+from app import db
 from . import main
 from flask_login import login_required,current_user
 from ..models import User,BlogPost,Comment,Role
 from .forms import UpdateProfile,CommentForm,PostForm
+from .picture_handler import add_profile_pic
 
 
 #views
@@ -14,12 +16,35 @@ def index():
     return render_template('index.html',title='Home of the brave')
 
 
-@main.route('/profile',methods=['GET','POST'])
-def profile(uname):
+@main.route('/user/<uname>/profile_update',methods=['GET','POST'])
+@login_required
+def profile_update(uname):
     '''
-    view function for a user profile
+    view function to updtae a user profile
     '''
     form=UpdateProfile()
     if form.validate_on_submit():
-        
-    user=User.query.filter_by(username=uname).first()
+
+        if form.pictures.data:
+            username=current_user.username
+            pic=add_profile_pic(form.pictures.data,username)
+
+            #setting current user image to the new uploaded pic
+            current_user.profile_image=pic
+        #resetting user data
+        current_user.username=form.username.data
+        current_user.email=form.email.data 
+        db.session.commit()  
+        flash('Account Updated')
+        return redirect(url_for('.profile'))
+
+    elif request.method=='GET':
+        #user is not submitting anything
+        form.username.data=current_user.username
+        form.email.data=current_user.email
+    
+    #fetch the current profile image from the static folder and inject to the template    
+    profile_image=url_for('static',filename='photos/'+current_user.profile_image)  
+    return render_template('profile.html',profile_image=profile_image,form=form)  
+
+
